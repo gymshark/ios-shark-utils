b76058a5fc3ef71ff18ddc54f4c7775f171d6d55
Lee Burrows <>
2021-04-20 22:29:35 +0100

    Adds Threadsafe Property Wrapper implementation

--- Sources/SharkUtils/ThreadSafe.swift
+++ Sources/SharkUtils/ThreadSafe.swift
@@ -1,8 +1,13 @@
-//
-//  File.swift
-//  
-//
-//  Created by Lee Burrows on 20/04/2021.
-//
-
-import Foundation
+@propertyWrapper
+public struct ThreadSafe<T> {
+    private let writeSafe: WriteSafe
+    public var unsafeValue: T
+    public var wrappedValue: T {
+        get { writeSafe.perform { unsafeValue } }
+        set { writeSafe.perform { unsafeValue = newValue } }
+    }
+    public init(wrappedValue: T, writeSafe: WriteSafe = WriteSafe()) {
+        self.unsafeValue = wrappedValue
+        self.writeSafe = writeSafe
+    }
+}
