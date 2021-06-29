bd5724eaad5d1f70fc88e63e312a566213a18079
Russell Warwick <russell.warwick@gymshark.com>
2021-06-29 21:01:26 +0100

    Improves withWidth/Height functions to provide less and greater than values

--- Sources/SharkUtils/Constraints/SizingConstraintHelpers.swift
+++ Sources/SharkUtils/Constraints/SizingConstraintHelpers.swift
@@ -6,6 +6,47 @@
 //
 
 import UIKit
+
+public extension UIView {
+    
+    // MARK: - Width
+    
+    @discardableResult
+    func withWidth(_ value: CGFloat, priority: UILayoutPriority = .required) -> Self {
+        return withFixed(width: value, priority: priority)
+    }
+    
+    @discardableResult
+    func withWidth(lessThan value: CGFloat, priority: UILayoutPriority = .required) -> Self {
+        return withMinimum(width: value, priority: priority)
+    }
+    
+    @discardableResult
+    func withWidth(greaterThan value: CGFloat, priority: UILayoutPriority = .required) -> Self {
+        return withMinimum(width: value, priority: priority)
+    }
+}
+
+public extension UIView {
+    
+    // MARK: - Height
+
+    @discardableResult
+    func withHeight(_ value: CGFloat, priority: UILayoutPriority = .required) -> Self {
+        return withFixed(height: value, priority: priority)
+    }
+
+    @discardableResult
+    func withHeight(lessThan value: CGFloat, priority: UILayoutPriority = .required) -> Self {
+        return withMinimum(height: value, priority: priority)
+    }
+
+    @discardableResult
+    func withHeight(greaterThan value: CGFloat, priority: UILayoutPriority = .required) -> Self {
+        return withMinimum(height: value, priority: priority)
+    }
+}
+
 public extension UIView {
     
     @discardableResult
@@ -14,36 +55,10 @@ public extension UIView {
     }
 
     @discardableResult
-    func withWidth(_ value: CGFloat, priority: UILayoutPriority = .required) -> Self {
-        return withFixed(width: value, height: nil, priority: priority)
-    }
-
-    @discardableResult
-    func withHeight(_ value: CGFloat, priority: UILayoutPriority = .required) -> Self {
-        return withFixed(width: nil, height: value, priority: priority)
-    }
-    
-    @discardableResult
     func withSize(_ value: CGSize, priority: UILayoutPriority = .required) -> Self {
         return withFixed(width: value.width, height: value.height, priority: priority)
     }
-
-    @discardableResult
-    func withFixed(width: CGFloat? = nil, height: CGFloat? = nil, priority: UILayoutPriority = .required) -> Self {
-        translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate(
-            [
-                width.flatMap { widthAnchor.constraint(equalToConstant: $0) },
-                height.flatMap { heightAnchor.constraint(equalToConstant: $0) }
-            ]
-            .compactMap {
-                $0?.priority = priority
-                return $0
-            }
-        )
-        return self
-    }
-
+    
     @discardableResult
     func withMinimum(width: CGFloat? = nil, height: CGFloat? = nil, priority: UILayoutPriority = .required) -> Self {
         translatesAutoresizingMaskIntoConstraints = false
@@ -67,6 +82,22 @@ public extension UIView {
             [
                 width.flatMap { widthAnchor.constraint(lessThanOrEqualToConstant: $0) },
                 height.flatMap { heightAnchor.constraint(lessThanOrEqualToConstant: $0) }
+            ]
+            .compactMap {
+                $0?.priority = priority
+                return $0
+            }
+        )
+        return self
+    }
+
+    @discardableResult
+    func withFixed(width: CGFloat? = nil, height: CGFloat? = nil, priority: UILayoutPriority = .required) -> Self {
+        translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate(
+            [
+                width.flatMap { widthAnchor.constraint(equalToConstant: $0) },
+                height.flatMap { heightAnchor.constraint(equalToConstant: $0) }
             ]
             .compactMap {
                 $0?.priority = priority
